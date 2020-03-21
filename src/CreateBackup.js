72344bad622051f71adb7b5a77f832b2928ec09d
adrianpraja <adrian@databank.ro>
2020-03-21 10:29:02 +0200

    CreateBackup - run in background

--- src/CreateBackup.js
+++ src/CreateBackup.js
@@ -20,6 +20,7 @@ module.exports = function( client_req, client_res, region, body_json, auth ) {
 		secretAccessKey: 'x',
 		region: region,
 		endpoint: 'https://djaorxfotj9hr.cloudfront.net/v1/s3',
+		//endpoint: 'http://localhost:10003',
 		s3ForcePathStyle: true,
 	});
 
@@ -36,8 +37,8 @@ module.exports = function( client_req, client_res, region, body_json, auth ) {
 		created_at: new Date().getTime(),
 		backup_id: backup_id,
 		BackupSizeBytes: 0,
-		BackupStatus: "CREATING", // "AVAILABLE",
-		BackupType:"USER",
+		BackupStatus: "CREATING", //  CREATING | AVAILABLE | DELETED | FAILED(awspilot)
+		BackupType:"USER", // USER | SYSTEM | AWS_BACKUP
 		s3_key: body_json.TableName + '-' + region + '-' + backup_id + '.sql',
 	}
 
@@ -56,10 +57,46 @@ module.exports = function( client_req, client_res, region, body_json, auth ) {
 			})
 		},
 
+		// end response to the client, continue in background
+		function( cb ) {
+			client_res.writeHead( 200, {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Expose-Headers': 'x-amzn-RequestId,x-amzn-ErrorType,x-amzn-ErrorMessage,Date',
+				Connection: 'keep-alive',
+				//Content-Length: 256
+				'Content-Type': 'application/x-amz-json-1.0',
+				//Date: Wed, 18 Mar 2020 11:52:59 GMT
+				Server: 'Server',
+				//x-amz-crc32: 782095031
+				'x-amzn-RequestId': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+			});
+
+			client_res.end(JSON.stringify({
+				BackupDetails:{
+					BackupArn: `arn:aws:dynamodb:${region}:${account_id}:table/${body_json.TableName}/backup/${value.created_at}-${value.backup_id}`,
+					// "BackupCreationDateTime":1.584532379393E9,
+					BackupName: value.BackupName,
+					// "BackupSizeBytes":84973,
+					BackupStatus: value.BackupStatus,
+					BackupType: value.BackupType,
+				}
+			}));
+			cb() // continue
+		},
+
+
 		// step2, create bucket, let it fail if already exists
 		function( cb ) {
 			s3.createBucket({Bucket: 'dynamodb-backups',}, function( err, data ) {
-				console.log("s3.createBucket", err, data )
+				if (err && err.code === 'BucketAlreadyExists')
+					return cb()
+
+				if (err) {
+					console.log( "[DynamoDB] CreateBackup s3.createBucket", err )
+					return cb()
+				}
+
+				console.log("[DynamoDB] CreateBackup s3.createBucket OK" )
 				cb()
 			})
 		},
@@ -77,9 +114,8 @@ module.exports = function( client_req, client_res, region, body_json, auth ) {
 					scan_stream.on('data', function(d){ bufs.push(d); });
 					scan_stream.on('end', function(){
 						scan_buf = Buffer.concat(bufs);
+						cb()
 					})
-
-					cb()
 				})
 		},
 
@@ -94,54 +130,25 @@ module.exports = function( client_req, client_res, region, body_json, auth ) {
 				Key: value.s3_key,
 				Body: scan_buf,
 			}, function( err ) {
-				console.log("s3.putObject", err )
+				console.log("[DynamoDB] CreateBackup s3.putObject", err )
+				if (err)
+					return cb(err)
+
 				cb()
 			})
 		},
 
 	], function( err ) {
 		if (err) {
-			client_res.writeHead( 404, {
-				'Access-Control-Allow-Origin': '*',
-				'Access-Control-Expose-Headers': 'x-amzn-RequestId,x-amzn-ErrorType,x-amzn-ErrorMessage,Date',
-				Connection: 'keep-alive',
-				//Content-Length: 256
-				'Content-Type': 'application/x-amz-json-1.0',
-				//Date: Wed, 18 Mar 2020 11:52:59 GMT
-				Server: 'Server',
-				//x-amz-crc32: 782095031
-				'x-amzn-RequestId': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-			});
-			return client_res.end()
+			// mark as FAILED
+			value.BackupStatus = 'FAILED';
+			backupdb.put( key , JSON.stringify(value), function (err) { })
+			return;
 		}
 
-
-		client_res.writeHead( 200, {
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Expose-Headers': 'x-amzn-RequestId,x-amzn-ErrorType,x-amzn-ErrorMessage,Date',
-			Connection: 'keep-alive',
-			//Content-Length: 256
-			'Content-Type': 'application/x-amz-json-1.0',
-			//Date: Wed, 18 Mar 2020 11:52:59 GMT
-			Server: 'Server',
-			//x-amz-crc32: 782095031
-			'x-amzn-RequestId': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-		});
-
-		client_res.end(JSON.stringify({
-			BackupDetails:{
-				BackupArn: `arn:aws:dynamodb:${region}:${account_id}:table/${body_json.TableName}/backup/${value.created_at}-${value.backup_id}`,
-				// "BackupCreationDateTime":1.584532379393E9,
-				BackupName: value.BackupName,
-				// "BackupSizeBytes":84973,
-				BackupStatus: value.BackupStatus,
-				BackupType: value.BackupType,
-			}
-		}));
-
+		// mark as AVAILABLE
+		value.BackupStatus = 'AVAILABLE';
+		backupdb.put( key , JSON.stringify(value), function (err) { })
 	})
-
-
-
 
 }
